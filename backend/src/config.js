6a179537de400a28ffa961a377a5183ec841120e
Eric Green <ericgreen.developer@gmail.com>
2023-08-04 11:40:49 -0400

    Configure the MongoDB connection

--- backend/src/config.js
+++ backend/src/config.js
@@ -1,2 +1,14 @@
-/** Define the port that the web service should listen on */
+/* Define the port that the web service should listen on */
 export const SERVICE_PORT = process.env.SERVICE_PORT || 3001;
+
+/* Define the database name */
+export const DATABASE_NAME = process.env.DATABASE_NAME || 'todo';
+
+/* Define the database service hostname */
+export const DATABASE_HOST = process.env.DATABASE_HOST || '127.0.0.1';
+
+/* Define the database service port */
+export const DATABASE_PORT = process.env.SERVICE_PORT || 27017;
+
+/* Define the database connection URI */
+export const DATABASE_CONNECTION_URI = process.env.DATABASE_CONNECTION_URI || `mongodb://${DATABASE_HOST}:${DATABASE_PORT}/${DATABASE_NAME}`;
--- backend/src/index.js
+++ backend/src/index.js
@@ -1,7 +1,9 @@
-import { SERVICE_PORT } from './config.js';
+import mongoose from 'mongoose';
 import { app } from './app.js';
+import { DATABASE_CONNECTION_URI, SERVICE_PORT } from './config.js';
 
-/** Start the Express server */
-app.listen(SERVICE_PORT, () => {
+/* Start the Express server */
+app.listen(SERVICE_PORT, async () => {
+    await mongoose.connect(DATABASE_CONNECTION_URI);
     console.log(`HTTP server listening on port ${SERVICE_PORT}`);
 });
