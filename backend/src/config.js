ee342b784f41faf303813fefb0bb1f3dc97b4c9a
Eric Green <ericgreen.developer@gmail.com>
2023-08-04 17:40:13 -0400

    Add dotenv package and read database credentials from .env.local

--- backend/src/config.js
+++ backend/src/config.js
@@ -1,9 +1,6 @@
 /* Define the port that the web service should listen on */
 export const SERVICE_PORT = process.env.SERVICE_PORT || 3001;
 
-/** Define the API base path for handling requests */
-export const API_BASE_PATH = '/api';
-
 /* Define the database name */
 export const DATABASE_NAME = process.env.DATABASE_NAME || 'todo';
 
@@ -13,5 +10,11 @@ export const DATABASE_HOST = process.env.DATABASE_HOST || '127.0.0.1';
 /* Define the database service port */
 export const DATABASE_PORT = process.env.SERVICE_PORT || 27017;
 
+/* Define the database username */
+export const DATABASE_USERNAME = process.env.DATABASE_USERNAME;
+
+/* Define the database password */
+export const DATABASE_PASSWORD = process.env.DATABASE_PASSWORD;
+
 /* Define the database connection URI */
-export const DATABASE_CONNECTION_URI = process.env.DATABASE_CONNECTION_URI || `mongodb://${DATABASE_HOST}:${DATABASE_PORT}/${DATABASE_NAME}`;
+export const DATABASE_CONNECTION_URI = `mongodb://${DATABASE_USERNAME}:${DATABASE_PASSWORD}@${DATABASE_HOST}:${DATABASE_PORT}/${DATABASE_NAME}?authSource=admin`;
