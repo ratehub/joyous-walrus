51628bb1633629b54fb881d6c717e8ba95726e2b
Eric Green <ericgreen.developer@gmail.com>
2023-08-04 21:02:52 -0400

    Add tests for error handling

--- backend/tests/todo.test.js
+++ backend/tests/todo.test.js
@@ -16,6 +16,14 @@ const todoList = [
     }
 ];
 
+/* Roll around on the keyboard */
+const badData = {
+    id: { bad: 'input' },
+    name: 42,
+    isComplete: 'bad_data',
+    some_random_field: true
+};
+
 /* Initialize an in-memory server for testing our endpoints with */
 beforeAll(async () => {
     mongo = await MongoMemoryServer.create();
@@ -54,7 +62,7 @@ describe('GET /api', () => {
 
 describe('POST /api', () => {
     it('should create a new todo', async () => {
-        const data = { id: randomUUID(), name: 'new todo' };
+        const data = { id: randomUUID(), name: 'new todo', isComplete: false };
         const res = await request(app).post('/api').send(data);
 
         /* 201-Created for creating a new resource */
@@ -69,11 +77,23 @@ describe('POST /api', () => {
         expect(newTodo.name).toBe(data.name);
         expect(newTodo.isComplete).toBe(false);
     })
+
+    it('should validate user input', async () => {
+        const res = await request(app).post('/api').send(badData);
+
+         /* 400-Bad Request */
+         expect(res.statusCode).toBe(400);
+         /* Assert every field we got wrong is flagged */
+         expect(res.body.errors[0].path).toBe('id');
+         expect(res.body.errors[1].path).toBe('name');
+         expect(res.body.errors[2].path).toBe('isComplete');
+    });
 });
 
 describe('PUT /api/:id', () => {
+    const data = { ...todoList[0], name: 'updated todo', isComplete: true };
+
     it('should update an existing todo', async () => {
-        const data = { ...todoList[0], name: 'updated todo', isComplete: true };
         const res = await request(app).put(`/api/${data.id}`).send(data);
 
         /* 200-OK for updating existing resource */
@@ -84,7 +104,25 @@ describe('PUT /api/:id', () => {
         const updatedTodo = await Todo.findOne(data);
         expect(updatedTodo.name).toBe(data.name);
         expect(updatedTodo.isComplete).toBe(data.isComplete);
-    })
+    });
+
+    it('should validate user input', async () => {
+        const res = await request(app).put(`/api/${todoList[0].id}`).send(badData);
+
+         /* 400-Bad Request */
+         expect(res.statusCode).toBe(400);
+         // NOTE: this is duplicate code - I find its better to be explicit when testing 
+         expect(res.body.errors[0].path).toBe('id');
+         expect(res.body.errors[1].path).toBe('name');
+         expect(res.body.errors[2].path).toBe('isComplete');
+    });
+
+    it('should return not found if the item does not exist', async () => {
+        const res = await request(app).put(`/api/null`).send(data);
+
+        /* 404-Not Found */
+        expect(res.statusCode).toBe(404);
+    });
 });
 
 describe('DELETE /api/:id', () => {
@@ -97,6 +135,13 @@ describe('DELETE /api/:id', () => {
 
         /* Assert that we are getting nothing back */
         expect(res.body).toMatchObject({});
+    });
+
+    it('should return not found if the item does not exist', async () => {
+        const res = await request(app).delete(`/api/null`);
+
+        /* 404-Not Found */
+        expect(res.statusCode).toBe(404);
     });
 });
 
